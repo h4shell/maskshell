c2d8f7729b54f2d9deaa433faed4c86a0f6fbb5b
h4shell <60841743+h4shell@users.noreply.github.com>
2024-06-08 03:22:55 +0200

    Update maskshell.sh

--- maskshell.sh
+++ maskshell.sh
@@ -1,1 +1,43 @@
+# Bash Script for Hide Phishing URL Created by KP
 
+url_checker() {
+    if [ ! "${1//:*}" = http ]; then
+        if [ ! "${1//:*}" = https ]; then
+            echo -e "\e[31m[!] Invalid URL. Please use http or https.\e[0m"
+            exit 1
+        fi
+    fi
+}
+
+echo -n "Paste Phishing URL here (with http or https): "
+read phish
+url_checker $phish
+sleep 1
+echo "Processing and Modifing Phishing URL"
+echo ""
+short=$(curl -s https://is.gd/create.php\?format\=simple\&url\=${phish})
+shorter=${short#https://}
+echo -e "\n\e[1;31;42m ### Masking Domain ###\e[0m"
+echo 'Domain to mask the Phishing URL (with http or https), ex: https://google.com, http
+://anything.org) :'
+echo -en "\e[32m=>\e[0m "
+read mask
+url_checker $mask
+words=$mask
+if [[ -z "$words" ]]; then
+echo -e "\e[31m[!] No words.\e[0m"
+echo -e "\nGenerating MaskPhish Link...\n"
+final=$mask@$shorter
+echo -e "Here is the MaskPhish URL:\e[32m ${final} \e[0m\n"
+exit
+fi
+if [[ "$words" =~ " " ]]; then
+echo -e "\e[31m[!] Invalid words. Please avoid space.\e[0m"
+echo -e "\nGenerating MaskPhish Link...\n"
+final=$mask@$shorter
+echo -e "Here is the MaskPhish URL:\e[32m ${final} \e[0m\n"
+exit
+fi
+echo -e "\nGenerating MaskPhish Link...\n"
+final=$words@$shorter
+echo -e "Here is the MaskPhish URL:\e[32m ${final} \e[0m\n"
